31899fc6a17940206f9eb8f27be99228147c5845
Elmer Zapata <eszapata@grupobisa.com>
2023-05-29 15:45:22 -0400

    first commit

---
+++ TestProject3/GetPostsStepDefinitions.cs
@@ -0,0 +1,35 @@
+using RestSharp;
+using System;
+using System.Net;
+using TechTalk.SpecFlow;
+
+namespace TestProject3
+{
+    [Binding]
+    public class GetPostsStepDefinitions
+    {
+        RestClient client = new RestClient("http://localhost:3000");
+
+        RestRequest request = new RestRequest("posts/{postid}", Method.Get);
+
+        RestResponse response;
+
+        [Given(@"I have an id with value (.*)")]
+        public void GivenIHaveAnIdWithValue(int p0)
+        {
+            request.AddUrlSegment("postid", p0);
+        }
+
+        [When(@"I send a get request")]
+        public void WhenISendIGetRequest()
+        {
+            response = client.Execute(request);
+        }
+
+        [Then(@"I expected a valid code response")]
+        public void ThenIExpectedAValidCodeResponse()
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
---
+++ TestProject3/features/GetPosts.feature
@@ -0,0 +1,12 @@
+﻿Feature: Get posts
+
+A short summary of the feature
+
+@positive @smoke @regression @integration @JIRA-7777 
+Scenario: Get by id 
+	Given I have an id with value 1
+	When I send a get request
+	Then I expected a valid code response
+
+
+
